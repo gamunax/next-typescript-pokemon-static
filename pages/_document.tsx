d8aee1eee0248c1ce12a8de99c857a6ffb57566c
Jan Pierre <gamunaxx@gmail.com>
2022-05-26 23:01:33 -0500

    feat: 🎸 add settings

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,11 +1,10 @@
-import Document, {  Html, Head, Main, NextScript, DocumentContext  } from 'next/document'
-import {CssBaseline} from '@nextui-org/react';
+import Document, { DocumentContext, Head, Html, Main, NextScript } from 'next/document';
 
 class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
-    const initialProps = await Document.getInitialProps(ctx)
+    const initialProps = await Document.getInitialProps(ctx);
 
-    return initialProps
+    return initialProps;
   }
 
   render() {
@@ -17,8 +16,8 @@ class MyDocument extends Document {
           <NextScript />
         </body>
       </Html>
-    )
+    );
   }
 }
 
-export default MyDocument
+export default MyDocument;
