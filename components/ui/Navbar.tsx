510a6b975c602c8bdd5e42c83f66d778d1242176
Jan Pierre <gamunaxx@gmail.com>
2022-06-06 17:14:26 -0500

    feat: 🎸 create empty state favorites page

--- components/ui/Navbar.tsx
+++ components/ui/Navbar.tsx
@@ -1,5 +1,6 @@
-import { Spacer, Text, useTheme } from '@nextui-org/react';
+import { Link, Spacer, Text, useTheme } from '@nextui-org/react';
 import Image from 'next/image';
+import NextLink from 'next/link';
 
 export const Navbar = () => {
   const { theme, isDark } = useTheme();
@@ -22,14 +23,23 @@ export const Navbar = () => {
         width={70}
         height={70}
       />
-      <Text color="white" h2>
-        P
-      </Text>
-      <Text color="white" h3>
-        okemon
-      </Text>
+      <NextLink href="/" passHref>
+        <Link>
+          <Text color="white" h2>
+            P
+          </Text>
+          <Text color="white" h3>
+            okemon
+          </Text>
+        </Link>
+      </NextLink>
+
       <Spacer css={{ flex: 1 }} />
-      <Text color="white">Favoritos</Text>
+      <NextLink href="/favorites" passHref>
+        <Link>
+          <Text color="white">Favoritos</Text>
+        </Link>
+      </NextLink>
     </div>
   );
 };
