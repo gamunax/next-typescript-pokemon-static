56ea8eba34d9ef27033c2c4ac3590bc9f109d64f
Jan Pierre <gamunaxx@gmail.com>
2022-06-08 16:41:21 -0500

    feat: 🎸 add ISG - incremental static regeneration

--- pages/pokemon/[id].tsx
+++ pages/pokemon/[id].tsx
@@ -79,17 +79,29 @@ export const getStaticPaths: GetStaticPaths = async (ctx) => {
 
   return {
     paths: pokemons151,
-    fallback: false,
+    fallback: 'blocking', // --> ISR
+    // fallback: false, // --> SSG
   };
 };
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
   const { id } = params as { id: string };
+  const pokemon = await getPokemonInfo(id);
+
+  if (!pokemon) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false,
+      },
+    };
+  }
 
   return {
     props: {
-      pokemon: await getPokemonInfo(id),
+      pokemon,
     },
+    revalidate: 86400,
   };
 };
 
